32b4aa98c0b09bd954d22ed2d6820ea84905d9f2
Abstract Programmer <AbstractProggrammer@example.com>
2023-11-08 21:18:46 +0800

    change navbar to file

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,23 @@
-const mobileNavShow = document.querySelector('.mobile-res')
 const hamburgerMenuBtn = document.querySelector('.image-menu') 
 const hamburgerMenuBtnClose = document.querySelector('.image-menu-close')
-const main = document.querySelector('.main-container')
+const navShowAndHide = document.querySelector('.navlinks')
+const backgroundBlur = document.querySelector('.main-container')
+
 
 
 hamburgerMenuBtn.addEventListener('click', ()=>{
-    mobileNavShow.classList.toggle('active')
-    hamburgerMenuBtnClose.classList.toggle('show')
-    hamburgerMenuBtn.classList.toggle('hide')
-    main.classList.toggle('opacity')
+    navShowAndHide.classList.toggle('show')
+    hamburgerMenuBtn.classList.add('hideImage')
+    hamburgerMenuBtnClose.classList.add('showImage')
+    backgroundBlur.classList.toggle('backgoundColor')
+
 })
 
 hamburgerMenuBtnClose.addEventListener('click',()=>{
-    mobileNavShow.classList.toggle('active')
-    hamburgerMenuBtnClose.classList.remove('show')
-    hamburgerMenuBtn.classList.remove('hide')
-    main.classList.remove('opacity')
+    navShowAndHide.classList.remove('show')
+    hamburgerMenuBtn.classList.remove('hideImage')
+    hamburgerMenuBtnClose.classList.remove('showImage')
+    backgroundBlur.classList.remove('backgoundColor')
 })
+
 
